81d5919771f291b14e5081a5d9f07351c5332f64
Lars T. Kyllingstad <lars.kyllingstad@sintef.no>
2018-06-28 16:18:58 +0200

    Clean up test package

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -22,4 +22,4 @@ class FmilibraryTestConan(ConanFile):
     def test(self):
         if not tools.cross_building(self.settings):
             os.chdir("bin")
-            self.run(".%sexample" % os.sep)
+            self.run(".%stest" % os.sep)
