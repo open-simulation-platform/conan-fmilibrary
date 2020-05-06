fc81951092e4b284a0de6b9c46e66f5ccaa12233
Lars T. Kyllingstad <lars.kyllingstad@sintef.no>
2020-05-06 10:36:18 +0200

    Modernise conanfile

--- conanfile.py
+++ conanfile.py
@@ -1,12 +1,18 @@
 from conans import ConanFile, CMake, tools
 
 
-class FmilibraryConan(ConanFile):
-    name = "FMILibrary"
+class FMILibraryConan(ConanFile):
+    name = "fmilibrary"
     version = "2.0.3"
     license = "https://svn.jmodelica.org/FMILibrary/tags/2.0.3/LICENSE.md"
     url = "https://github.com/kyllingstad/conan-FMILibrary"
     description = "An implementation of the FMI standard which enables FMU import in applications"
+    scm = {
+        "type": "git",
+        "url": "https://github.com/modelon-community/fmi-library.git",
+        "revision": version,
+        "subfolder": "src"
+    }
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = "shared=False"
@@ -14,10 +20,8 @@ class FmilibraryConan(ConanFile):
     exports_sources = [ "build-static-c99snprintf.patch" ]
 
     def source(self):
-        tools.download("https://jmodelica.org/fmil/FMILibrary-2.0.3-src.zip", "FMILibrary-2.0.3-src.zip")
-        tools.unzip("FMILibrary-2.0.3-src.zip")
-        tools.patch(base_path="FMILibrary-2.0.3", patch_file="build-static-c99snprintf.patch")
-        tools.replace_in_file("FMILibrary-2.0.3/CMakeLists.txt", "project (FMILibrary)",
+        tools.patch(base_path="src", patch_file="build-static-c99snprintf.patch")
+        tools.replace_in_file("src/CMakeLists.txt", "project (FMILibrary)",
                               '''project (FMILibrary)
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()''')
@@ -25,7 +29,7 @@ conan_basic_setup()''')
     def build(self):
         cmake = CMake(self)
         cmake.configure(
-            source_folder="FMILibrary-2.0.3",
+            source_folder="src",
             defs={
                 "FMILIB_BUILD_STATIC_LIB": "OFF" if self.options.shared else "ON",
                 "FMILIB_BUILD_SHARED_LIB": "ON"  if self.options.shared else "OFF",
