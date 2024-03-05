10ef51e456e04330d84fcfbd2c755cb633668f40
Kristoffer Eide <37700866+eidekrist@users.noreply.github.com>
2024-03-05 13:56:09 +0100

    Conan v2 (#14)
    Update recipe to Conan 2.x.
    
    ---------
    
    Co-authored-by: kristoffere <kristoffer.eide@sintef.no>
    Co-authored-by: Lars T. Kyllingstad <lars.kyllingstad@sintef.no>

--- conanfile.py
+++ conanfile.py
@@ -1,54 +1,53 @@
-from conans import ConanFile, CMake, tools
+import os, pathlib
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import patch, copy
+from conan.tools.scm import Git
 
 
 class FMILibraryConan(ConanFile):
     name = "fmilibrary"
     version = "2.3"
     license = "https://github.com/modelon-community/fmi-library/blob/master/LICENSE.md"
-    url = "https://github.com/kyllingstad/conan-FMILibrary"
+    url = "https://github.com/open-simulation-platform/conan-fmilibrary"
     description = "An implementation of the FMI standard which enables FMU import in applications"
-    scm = {
-        "type": "git",
-        "url": "https://github.com/modelon-community/fmi-library.git",
-        "revision": version,
-        "subfolder": "src"
-    }
+    
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
-    default_options = "shared=False"
-    generators = "cmake"
-    exports_sources = [ "build-static-c99snprintf.patch" ]
+    default_options = {"shared": False}
+
+    tool_requires = "cmake/[>=3.15]"
+    generators = "CMakeDeps"
+
+    exports_sources = "build-static-c99snprintf.patch"
 
     def source(self):
-        tools.patch(base_path="src", patch_file="build-static-c99snprintf.patch")
-        tools.replace_in_file("src/CMakeLists.txt", "project (FMILibrary)",
-                              '''project (FMILibrary)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()''')
+        git = Git(self)
+        git.clone(url="https://github.com/modelon-community/fmi-library.git", target="src", args=["--branch=2.3"])
+        patch(self, base_path="src", patch_file="build-static-c99snprintf.patch")
 
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):       
+        tc = CMakeToolchain(self)
+        tc.cache_variables["FMILIB_BUILD_STATIC_LIB"] = not self.options.shared
+        tc.cache_variables["FMILIB_BUILD_SHARED_LIB"] = not not self.options.shared
+        tc.cache_variables["FMILIB_BUILD_TESTS"] = False
+        tc.cache_variables["FMILIB_GENERATE_DOXYGEN_DOC"] = False
+        tc.cache_variables["FMILIB_INSTALL_PREFIX"] = pathlib.Path(os.path.join(self.build_folder, "install")).as_posix()
+        tc.generate()
+    
     def build(self):
         cmake = CMake(self)
-        cmake.configure(
-            source_folder="src",
-            defs={
-                "FMILIB_BUILD_STATIC_LIB": "OFF" if self.options.shared else "ON",
-                "FMILIB_BUILD_SHARED_LIB": "ON"  if self.options.shared else "OFF",
-                "FMILIB_BUILD_TESTS": "OFF",
-                "FMILIB_INSTALL_PREFIX": self.build_folder + "/install",
-            })
+        cmake.configure(build_script_folder="src")
         cmake.build()
-        cmake.install()
 
     def package(self):
-        fmilib_install_dir = self.build_folder + "/install"
-        self.copy("*.h",    dst="include",  src=fmilib_install_dir+"/include")
-        self.copy("*.dll",  dst="bin",      src=fmilib_install_dir, keep_path=False)
-        self.copy("*.so",   dst="lib",      src=fmilib_install_dir, keep_path=False)
-        self.copy("*.dylib",dst="lib",      src=fmilib_install_dir, keep_path=False)
-        self.copy("*.lib",  dst="lib",      src=fmilib_install_dir, keep_path=False)
-        self.copy("*.a",    dst="lib",      src=fmilib_install_dir, keep_path=False)
-        self.copy("LICENSE.md",                  src="src", dst="licenses", keep_path=False)
-        self.copy("FMILIB_Acknowledgements.txt", src="src", dst="licenses", keep_path=False)
+        cmake = CMake(self)
+        cmake.install()
+        copy(self, "*.dll", os.path.join(self.build_folder, str(self.settings.build_type)), os.path.join(self.package_folder, "bin"))
 
     def package_info(self):
         if self.options.shared:
